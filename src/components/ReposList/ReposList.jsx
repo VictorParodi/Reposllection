2156e398a0e5021c5b5a8cff0755b608c224018f
Victor Parodi <victorparodi@Victors-MacBook-Pro.local>
2020-07-05 20:13:39 -0500

    [F-004] Building a static list of repos

--- src/components/ReposList/ReposList.jsx
+++ src/components/ReposList/ReposList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchRepos } from './../../actions';
+import { Icon, Menu, Table, Label } from 'semantic-ui-react';
 
 class ReposList extends React.Component {
     componentDidMount() {
@@ -9,7 +10,65 @@ class ReposList extends React.Component {
 
     render() {
         return (
-            <h1>Repos List</h1>
+            <Table celled>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Name</Table.HeaderCell>
+                        <Table.HeaderCell>Description</Table.HeaderCell>
+                        <Table.HeaderCell>Language</Table.HeaderCell>
+                        <Table.HeaderCell>Default branch</Table.HeaderCell>
+                        <Table.HeaderCell>Git url</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    <Table.Row>
+                        <Table.Cell>
+                            <Label ribbon>Repo name</Label>
+                        </Table.Cell>
+                        <Table.Cell>Repo data</Table.Cell>
+                        <Table.Cell>Repo data</Table.Cell>
+                        <Table.Cell>Repo data</Table.Cell>
+                        <Table.Cell>Repo data</Table.Cell>
+                    </Table.Row>
+
+                    <Table.Row>
+                        <Table.Cell>
+                            <Label ribbon>Repo name</Label>
+                        </Table.Cell>
+                        <Table.Cell>Repo data</Table.Cell>
+                        <Table.Cell>Repo data</Table.Cell>
+                        <Table.Cell>Repo data</Table.Cell>
+                        <Table.Cell>Repo data</Table.Cell>
+                    </Table.Row>
+
+                    <Table.Row>
+                        <Table.Cell>
+                            <Label ribbon>Repo name</Label>
+                        </Table.Cell>
+                        <Table.Cell>Repo data</Table.Cell>
+                        <Table.Cell>Repo data</Table.Cell>
+                        <Table.Cell>Repo data</Table.Cell>
+                        <Table.Cell>Repo data</Table.Cell>
+                    </Table.Row>
+                </Table.Body>
+
+                <Table.Footer>
+                    <Table.Row>
+                        <Table.HeaderCell colSpan='5'>
+                            <Menu floated='right' pagination>
+                                <Menu.Item as='a' icon>
+                                    <Icon name='chevron left' />
+                                </Menu.Item>
+                                
+                                <Menu.Item as='a' icon>
+                                    <Icon name='chevron right' />
+                                </Menu.Item>
+                            </Menu>
+                        </Table.HeaderCell>
+                    </Table.Row>
+                </Table.Footer>
+            </Table>
         );
     }
 }
